2cd5f96de324fa3a4711ca4daf2077c74ad7a913
chrisbrickey <cbrickey1@gmail.com>
2017-04-06 15:38:04 -0700

    fixed tests, routed 'Home' to root, added images

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -34,7 +34,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' } THIS LINE WAS REPLACED WITH THE ABOVE LINE WHEN I ADDED A CART BUTTON
         format.json { render :show, status: :created, location: @line_item }
       else
