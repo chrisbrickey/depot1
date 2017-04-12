b38b7a40e17106632e807f77cda7de6127729ef2
chrisbrickey <cbrickey1@gmail.com>
2017-04-12 12:15:59 -0700

    Added admin user model and tests

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  #below line checks that, when initialized, user has a name present and that it is uniqueness
+  validates :name, presence: true, uniqueness: true
+
+  #below line makes user re-enter password in second field and checks that passwords match
+  #...it was added when I specified password:digest when I created the User model
+  has_secure_password
+end
