cf67e30b332a45d07168093ffcd53f170d3632e2
chrisbrickey <cbrickey1@gmail.com>
2017-04-12 22:14:24 -0700

    used transaction to prevent deletion of last admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
   #below line makes user re-enter password in second field and checks that passwords match
   #...it was added when I specified password:digest when I created the User model
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError
+  end
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise Error.new "Can't delete last user"
+      end
+    end
+
 end
