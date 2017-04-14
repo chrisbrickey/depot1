3267e610324dd4d3f5d6d7b2e91bf68039a4fe1e
chrisbrickey <cbrickey1@gmail.com>
2017-04-14 10:56:11 -0700

    added toggle for language but errors

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,6 +6,12 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.order(:title)
+    #redirects the store path for a locale (translation) IF a locale has been set
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    #original code (for when locale is not set)
+    else
+      @products = Product.order(:title)
+    end
   end
 end
